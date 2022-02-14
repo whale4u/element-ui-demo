4cb04480ef22e5a4d2ae556f32d15b2b8e199107
jidda <jidda.gb@foxmail.com>
2022-02-14 22:49:13 +0800

    优化部分逻辑

--- src/pages/common/index.vue
+++ src/pages/common/index.vue
@@ -1,25 +1,23 @@
 <template>
   <div >
     <!-- <home></home> -->
-  <el-header>
     <el-menu
-      :default-active="this.$route.path" 
-      router mode="horizontal"
-      class="el-menu-vertical-demo"
-      @open="handleOpen"
-      @close="handleClose"
-      background-color="#545c64"
-      text-color="#fff"
-      active-text-color="#ffd04b">
-      <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
-        <template slot="title">
-          <i class="el-icon-s-platform"></i>
-          <span> {{ item.navItem }}</span>
-        </template>
-      </el-menu-item>
-      <el-button type="info" @click="logout">退出</el-button>
-    </el-menu>
-    </el-header>
+        :default-active="this.$route.path" 
+        router mode="horizontal"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
+          <template slot="title">
+            <i class="el-icon-s-platform"></i>
+            <span> {{ item.navItem }}</span>
+          </template>
+        </el-menu-item>
+        <!-- <el-button type="info" @click="logout">退出</el-button> -->
+      </el-menu>
     <router-view/>
   </div>
 </template>
@@ -31,10 +29,10 @@ export default {
       return {
          navList:[ 
             {name:'/',navItem:'主页'}, 
-            {name:'/components/Management',navItem:'xxx'},
             {name:'/passwd',navItem:'密码库'}, 
-            {name:'/user',navItem:'用户设置'}, 
+            {name:'/user',navItem:'用户管理'}, 
             {name:'/login',navItem:'登陆'}, 
+            {name:'/logout',navItem:'退出'}, 
         ],
       }
   },
@@ -45,6 +43,7 @@ export default {
     handleClose(key, keyPath) {
       console.log(key, keyPath);
     },
+    // 退出逻辑
     logout() {
       alert("logout!")
       localStorage.removeItem('token')
